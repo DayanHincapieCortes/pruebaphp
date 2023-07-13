fb421d3615ee371d7499a8c70b9fa0fb5ba10e35
DayanHincapieCortes <139444059+DayanHincapieCortes@users.noreply.github.com>
2023-07-13 11:55:05 -0500

    Update productoSQL.sql

--- productoSQL.sql
+++ productoSQL.sql
@@ -10,3 +10,7 @@ CREATE TABLE producto(
     Fecha_de_creacion DATE NOT NULL
 );
 
+INSERT INTO producto VALUES ('1','ejemplo1','a',100,1,'cafe1',10,'2023-07-13');
+INSERT INTO producto VALUES ('2','ejemplo2','b',200,2,'cafe2',20,'2023-07-13');
+INSERT INTO producto VALUES ('3','ejemplo3','c',300,3,'cafe3',30,'2023-07-13');
+INSERT INTO producto VALUES ('4','ejemplo4','d',400,4,'cafe4',40,'2023-07-13');
