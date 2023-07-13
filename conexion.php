6bc1008805d39072df68dd03bdf7099f7b14cb4d
DayanHincapieCortes <139444059+DayanHincapieCortes@users.noreply.github.com>
2023-07-13 03:18:19 -0500

    Delete conexion.php

--- conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $passwd = "";
-    $database = "pruebaphp";
-    $conexion = mysqli_connect($host, $user, $passwd,$database);
-    mysqli_set_charset($conexion, "utf8");
